9c0e9735de2e0e10daef6166579d87f2515173c1
Vladyslav Kramarenko <vkramarenko.at.work@gmail.com>
2025-01-19 01:16:34 -0300

    heroku deploy

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: ' http://127.0.0.1:8000/', // Replace with your actual base URL
+    baseURL: process.env.REACT_APP_BACKEND_BASE_URL, // Replace with your actual base URL
 });
 
 export default axiosInstance;
